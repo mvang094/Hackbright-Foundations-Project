217e8b7b5982f4a5455f8a23b7e6f5f20e4ad8bf
Mvang094 <mvang094@gmail.com>
2022-09-09 15:31:45 -0500

    Working Riddles

--- public/index.js
+++ public/index.js
@@ -1,10 +1,21 @@
+let ridAnswer, ridHint;
+
 const playBtn2 = document.querySelector('.play2');
+const yesBtn = document.querySelector('#yes');
+const noBtn = document.querySelector('#no');
+
 const riddleBox = document.querySelector('.riddle-box');
 const riddleForm = document.querySelector('#riddleForm');
 
+const tableBox = document.querySelector("maze-T");
+const tableBody = document.querySelector("maze-tBody");
+
 const threeBoxes = document.querySelector('.three-boxes');
+const mazeBox = document.querySelector("A-maze-zing");
+const replayDiv = document.querySelector(".replayDiv");
 
 riddleBox.classList.add('hide');
+replayDiv.classList.add('hide');
 
 const baseURL = 'http://localhost:5010/api/project';
 
@@ -13,7 +24,9 @@ function riddler(){
     .then(res => {
         const data = res.data;
         console.log(data);
-        guessRiddle(data);
+        // if (data === "")
+            //alert a div
+            guessRiddle(data)
     })
 }
 
@@ -21,38 +34,114 @@ function guessRiddle(value){
     
     threeBoxes.classList.add('hide');
     riddleBox.classList.remove('hide');
-    // riddleBox.classList.add('riddling');
 
     const {question} = value;
-    const {answer} = value;
+    ridAnswer = value.answer;
+    ridHint = value.hint;
 
     const riddleLabel = document.querySelector("#rr");
     riddleLabel.textContent = `${question}`;
 
-    riddleBox.addEventListener("submit", checkAnswer(answer));    
+    const riddleAnswer = document.createElement('input');
+    riddleAnswer.setAttribute("type", "text");
+    riddleAnswer.setAttribute("id", "riddlerAnswer");
+    riddleAnswer.setAttribute("name", "riddlerAnswer");
+    const riddleBtn = document.createElement('input');
+    riddleBtn.setAttribute("type", "submit");
+    riddleBtn.setAttribute("id", "ridBtn");
+    riddleBtn.setAttribute("value", "Submit");
     
-    //Issue: how to get the answer dynamically?
-    //something is making it submit before I can submit
-    //How do I compare input with solution?
+    riddleForm.appendChild(riddleAnswer);
+    riddleForm.appendChild(riddleBtn);
+    
+    riddleBox.addEventListener("submit", getAnswer);
 }
 
-// function checkAnswer(solution){
+function getAnswer(event){
+    event.preventDefault();
+
+    let wrongCount = 0;
+
+    let inputAnswer = document.querySelector('#riddlerAnswer');
+    let inputValue = inputAnswer.value;
+    console.log(inputValue);
+
+    let lowerInputAnswer = inputValue.toLowerCase();
     
-//     // let rAnswer = document.querySelector("#riddle-answer");
-//     // let rValue = rAnswer.value;
-//     let riddleSoln = solution;
-//     let data = ridAnswer.toLowerCase();
-//     console.log(riddleSoln)
-//     console.log(data);
+    if(lowerInputAnswer === ridAnswer)
+    {
+        console.log('Hurray!');
+        riddleBox.classList.add('hide');
+        replayDiv.classList.remove('hide');
+        inputAnswer = "";
+        ridAnswer = "";
+        ridHint = "";
+        let removeBtn = document.querySelector('#ridBtn');
+        let removeAnswer = document.querySelector('#riddlerAnswer');
+        riddleForm.removeChild(removeBtn);
+        riddleForm.removeChild(removeAnswer);
+
+        yesBtn.addEventListener("click", again(ridAnswer));
+        noBtn.addEventListener("click", close); 
+    }
+    else
+    {
+            wrongChoice();
+            wrongCount++;
+    }
     
-//     if(data === riddleSoln)
-//     {
-//         rightChoiceRectangle;
-//     }
-//    else
-//    {
-//         alert("try again");
-//         guessRiddle();
-//    }
+    // if(wrongCount === 3)
+}
+
+function wrongChoice(){
+    alert("Hmm...Not quite");
+}
+
+function again(){
+    replayDiv.classList.add('hide');
+
+
+
+    riddler();
+}
+
+function close(){
+    replayDiv.classList.add('hide');
+    threeBoxes.classList.remove('hide');
+}
+
+function createMaze(){
+    let rowIndex, colIndex;
+    let tableLength = 9;
+    for (rowIndex = 0; rowIndex < tableLength; rowIndex++){
+        const row = document.createElement("tr");
+
+        for(colIndex = 0; colIndex < tableLength; colIndex++){
+            const col = document.createElement("td");
+            col.setAttribute("id", "columns");
+            col.classList.add("columns");
+            if(rowIndex === 0 && colIndex ===0)
+            {
+                col.setAttribute("type", "start");
+                col.setAttribute("id", "starts");
+                col.classList.add("begin");
+            }
+            else if(rowIndex === tableLength && colIndex === tableLength)
+            {
+                col.style.backgroundColor = "rgb(244,0,0)";
+                col.setAttribute("type", "finish");
+                col.setAttribute("id", "finishes");
+                col.classList.add("end");
+            }
+        }
+        row.appendChild(col);
+    }
+
+
+
+
+
+
+}
 
 playBtn2.addEventListener('click', riddler);
--- server/data.js
+++ server/data.js
@@ -2,28 +2,33 @@ const riddles =[
     {
         id: 0,
         question: "What has to be broken before you can use it?",
-        answer: "eggs"
+        answer: "eggs",
+        hint: "breakfast"
     },
 
     {
         id: 1,
         question: "What is always in front of you but can’t be seen?",
-        answer: "future"
+        answer: "future",
+        hint: "past, present..."
     },
     {
         id: 2,
         question: "What can you break, even if you never pick it up or touch it?",
-        answer: "promise"
+        answer: "promise",
+        hint: "pinky"
     },
     {
         id: 3,
         question: "I shave every day, but my beard stays the same. What am I?",
-        answer: "barber"
+        answer: "barber",
+        hint: "person"
     },
     {
         id: 4,
         question:  "I am the beginning of everything, the end of everywhere. I'm the beginning of eternity, the end of time and space. What am I?",
-        answer: "e"
+        answer: "e",
+        hint: "letter"
     }
     
 ]
